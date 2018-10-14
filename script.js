2f8e5958bbe7fc3cd46c4a3aafbf54154bd48673
vtrev <baloyi.trev@gmail.com>
2018-10-14 16:23:52 +0200

    added the page and js

---
+++ script.js
@@ -0,0 +1,34 @@
+// dzi html elements 
+let messageTag = document.querySelector('#admin-input')
+let headingTag = document.querySelector('#heading')
+
+// on the user's side
+let userHeading = document.querySelector('.user-heading');
+let userMessage = document.querySelector('.user-message');
+
+
+// function ine ya do post that message
+
+const postMessage = function () {
+
+    // do not post empty messages
+    //Ragex would be better than this if shit,
+    if (messageTag.value !== "" && headingTag !== "") {
+        // grab the value of the inputs from the DOM
+        // set the innerHTML of the user side
+        userHeading.innerHTML = headingTag.value;
+        userMessage.innerHTML = messageTag.value;
+        //clear the input fields after submit
+        headingTag.value = "";
+        messageTag.value = "";
+        // return false to avoid the page from refreshing
+        return false
+    } else {
+        alert('Please enter a valid heading and message first!')
+    }
+}
+
+//this is a very shit way of doing it but it uses plain JS HTML and CSS,if you want to havev 5 
+//posts it would mean haveing 10 divs, and 20 elements on the JS and 20 statements 
+//to set values 
+//so templating would be perfect
